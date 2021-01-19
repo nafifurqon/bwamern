554e37217cf6e10f5dce036f1b95d452d4264155
nafifurqon <muhammadnafi98@gmail.com>
2021-01-19 18:14:59 +0700

    change node-sass 5.0.0 to 4.13.1 and create the Header parts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "assets/scss/style.scss";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import "assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      hai dunia
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
     </div>
   );
 }
