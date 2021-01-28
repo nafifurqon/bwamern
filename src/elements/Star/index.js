d39ffc536db2cc4942c3f8d319cbc656290311c5
nafifurqon <muhammadnafi98@gmail.com>
2021-01-28 21:59:02 +0700

    create star elements and some function for star elements as smart component

---
+++ src/elements/Star/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import propTypes from 'prop-types'
+
+export default function Star({value, height, width, spacing}) {
+    const decimals = Number(value) % 1;
+    
+    const star = [];
+    let leftPos = 0;
+    for (let index = 0; index < 5 && index < value - decimals && index; index++) {
+        leftPos = leftPos + width;
+        star.push(
+            <div 
+                className="star"
+                key={`star-${index}`}
+                style={{left: index * width, width: width, marginRight: spacing}}
+            ></div>
+        );
+    }
+    if(decimals > 0 && value <= 5) {
+        star.push(
+            <div 
+                className="star"
+                key={`star-${index}`}
+                style={{left: leftPos, width: width - spacing}}
+            ></div>
+        );
+    }
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Star.propTypes = {
+    className: propTypes.string,
+    value: propTypes.number,
+    height: propTypes.number,
+    width: propTypes.number,
+    spacing: propTypes.number
+}
