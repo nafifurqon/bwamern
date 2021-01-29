81b9e8a2b96b7c355e029fe0afc9dae1c2d3818d
nafifurqon <muhammadnafi98@gmail.com>
2021-01-29 10:22:50 +0700

    create testimony part markups

--- src/parts/Testimony.js
+++ src/parts/Testimony.js
@@ -1,10 +1,51 @@
 import React from 'react'
 import Star from 'elements/Star'
+import TestimonyAccent from 'assets/images/img-testimonial-frame.jpg'
+import Button from 'elements/Button'
 
-export default function Testimony() {
+export default function Testimony({data}) {
     return (
-        <div>
-            <Star value={2.5} height={35} width={35} spacing={4}></Star>
-        </div>
+        <section className="container">
+            <div className="row align-items-center">
+                <div className="col-auto" style={{ marginRight: 70 }}>
+                    <div className="testimonial-hero" style={{ margin: `30px 0 0 30px` }}>
+                        <img 
+                            src={data.imageUrl} 
+                            alt="Testimonial"
+                            className="position-absolute" 
+                            style={{ zIndex: 1 }}
+                        />
+                        <img 
+                            src={TestimonyAccent}
+                            alt="Testimonial"
+                            className="position-absolute" 
+                            style={{ margin: `30px 0 0 30px`, zIndex: 1 }}
+                        />
+                    </div>
+                </div>
+                <div className="col-6">
+                    <h4 style={{ marginBottom: 40 }}>{data.name}</h4>
+                    <Star value={data.rate} height={35} width={35} spacing={4}></Star>
+                    <h5 className="h2 font-weight-light line-height-2 my-3">
+                        {data.content}
+                    </h5>
+                    <span className="text-gray-500">{data.familyName}, {data.familyOccupation}</span>
+                    <div>
+                        <Button 
+                            className="btn px-5" 
+                            style={{ marginTop: 40 }}
+                            hasShadow
+                            isPrimary
+                            type="button"
+                            href={`/testimonial/${data._id}`}
+                        >
+                            Read Their Story
+                        </Button>
+                    </div>
+                    
+                </div>
+            </div>
+            
+        </section>
     )
 }
