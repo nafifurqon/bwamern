82a9e903a38f0622bf94c821495a194fc8831824
nafifurqon <muhammadnafi98@gmail.com>
2021-01-29 10:39:26 +0700

    finished styling testimonial parts

--- src/parts/Testimony.js
+++ src/parts/Testimony.js
@@ -7,7 +7,7 @@ export default function Testimony({data}) {
     return (
         <section className="container">
             <div className="row align-items-center">
-                <div className="col-auto" style={{ marginRight: 70 }}>
+                <div className="col-auto" style={{ marginRight: 60 }}>
                     <div className="testimonial-hero" style={{ margin: `30px 0 0 30px` }}>
                         <img 
                             src={data.imageUrl} 
@@ -19,11 +19,11 @@ export default function Testimony({data}) {
                             src={TestimonyAccent}
                             alt="Testimonial"
                             className="position-absolute" 
-                            style={{ margin: `30px 0 0 30px`, zIndex: 1 }}
+                            style={{ margin: `-30px 0 0 -30px` }}
                         />
                     </div>
                 </div>
-                <div className="col-6">
+                <div className="col">
                     <h4 style={{ marginBottom: 40 }}>{data.name}</h4>
                     <Star value={data.rate} height={35} width={35} spacing={4}></Star>
                     <h5 className="h2 font-weight-light line-height-2 my-3">
@@ -36,7 +36,7 @@ export default function Testimony({data}) {
                             style={{ marginTop: 40 }}
                             hasShadow
                             isPrimary
-                            type="button"
+                            type="link"
                             href={`/testimonial/${data._id}`}
                         >
                             Read Their Story
