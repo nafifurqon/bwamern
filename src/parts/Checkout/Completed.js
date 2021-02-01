601795195b40500161096f610ddb58e2d69c565f
nafifurqon <muhammadnafi98@gmail.com>
2021-02-01 20:42:21 +0700

    create Completed parts

---
+++ src/parts/Checkout/Completed.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Fade from 'react-reveal/Fade'
+import CompletedIllustration from 'assets/images/img-hero.jpg'
+
+export default function Completed() {
+    return (
+        <Fade>
+            <div className="container" style={{marginBottom: 30}}>
+                <div className="row justify-content-center text-center">
+                    <div className="col-4">
+                        <img 
+                            src={CompletedIllustration}
+                            alt="completed checkout apartment"
+                            className="img-fluid"
+                        />
+                        <p className="text-gray-500">
+                            We will inform you via email later once the transaction has been accepted
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </Fade>
+    )
+}
