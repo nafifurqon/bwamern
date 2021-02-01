75e75968e8462065e906075bd2dcc1c9f9fe01e5
nafifurqon <muhammadnafi98@gmail.com>
2021-02-01 14:54:27 +0700

    created InputText elements

--- src/elements/Form/index.js
+++ src/elements/Form/index.js
@@ -1,4 +1,5 @@
 import InputDate from './InputDate';
 import InputNumber from './InputNumber';
+import InputText from './InputText';
 
-export { InputNumber, InputDate };
+export { InputNumber, InputDate, InputText };
