f2fcc1b33ce5b207f5fc9fa5a1295892c9a705f7
nafifurqon <muhammadnafi98@gmail.com>
2021-03-04 11:03:09 +0700

    create get data from landing page from api

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Header from 'parts/Header';
-import landingPage from 'json/landingPage.json';
 import Hero from 'parts/Hero';
 import MostPicked from 'parts/MostPicked';
 import Categories from 'parts/Categories';
 import Testimony from 'parts/Testimony';
-import Footer from 'parts/Footer'
+import Footer from 'parts/Footer';
+import { connect } from 'react-redux';
+import { fetchPage } from 'store/actions/page';
 
-export default class LandingPage extends Component {
+class LandingPage extends Component {
     constructor(props) {
         super(props);
         this.refMostPicked = React.createRef();
@@ -16,24 +17,36 @@ export default class LandingPage extends Component {
     componentDidMount() {
         window.title = "Staycation | Home";
         window.scrollTo(0,0);
+        
+        if(!this.props.page.landingPage){
+            this.props.fetchPage(`https://nafi-staycation-backend.herokuapp.com/api/v1/member/landing-page`, 'landingPage')
+        }
     };
 
     render() {
+        const { page } = this.props;
+
         return (
             <>
                 <Header {...this.props}></Header>
                 <Hero 
                     refMostPicked={this.refMostPicked}
-                    data={landingPage.hero} 
+                    data={page.hero} 
                 />
                 <MostPicked 
                     refMostPicked={this.refMostPicked}
-                    data={landingPage.mostPicked} 
+                    data={page.mostPicked} 
                 />
-                <Categories data={landingPage.categories}  />
-                <Testimony data={landingPage.testimonial} />
+                <Categories data={page.categories}  />
+                <Testimony data={page.testimonial} />
                 <Footer />
             </>
         )   
     }
 }
+
+const mapStateToProps = (state) => ({
+    page: state.page ? state.page.landingPage : null
+})
+
+export default connect(mapStateToProps, {fetchPage})(LandingPage)
