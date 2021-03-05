9f5a26c6a79bc2be2e7a98ee4310b2ac24e868d0
nafifurqon <muhammadnafi98@gmail.com>
2021-03-05 10:27:15 +0700

    integrate get data API at details page until checkout booking

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -42,7 +42,7 @@ class Checkout extends Component {
 
     render() {
         const { data } = this.state;
-        const { checkout } = this.props;
+        const { checkout, page } = this.props;
 
         if(!checkout) return (
             <div className="container">
@@ -53,7 +53,12 @@ class Checkout extends Component {
                     <div className="col-3">
                         Pilih kamar dulu
                         <div>
-                            <Button className="btn mt-5" type="link" href="/" isLight>
+                            <Button 
+                                className="btn mt-5" 
+                                type="button" 
+                                onClick={() => this.props.history.goBack()}
+                                isLight
+                            >
                                 Back
                             </Button>
                         </div>
@@ -70,7 +75,7 @@ class Checkout extends Component {
                     <BookingInformation
                         data={data}
                         checkout={checkout}
-                        ItemDetails={ItemDetails}
+                        ItemDetails={page[checkout._id]}
                         onChange={this.onChange}
                     />
                 )
@@ -81,7 +86,7 @@ class Checkout extends Component {
                 content: (
                     <Payment
                         data={data}
-                        ItemDetails={ItemDetails}
+                        ItemDetails={page[checkout._id]}
                         checkout={checkout}
                         onChange={this.onChange}
                     />
@@ -197,7 +202,8 @@ class Checkout extends Component {
 }
 
 const mapStateToProps = (state) => ({
-    checkout: state.checkout
+    checkout: state.checkout,
+    page: state.page
 });
 
 export default connect(mapStateToProps)(Checkout);
